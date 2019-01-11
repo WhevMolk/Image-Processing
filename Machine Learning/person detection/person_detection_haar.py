03c5b304b815f5dd219eb1d491280717c24900ee
Manuel Olk <manuel.olk@gmx.de>
2019-01-11 13:13:49 +0100

    Changed output of detection algorithm to in task given format

--- Machine Learning/person detection/person_detection_haar.py
+++ Machine Learning/person detection/person_detection_haar.py
@@ -13,27 +13,31 @@ import glob
 
 casc_body_path = "haarcascade_fullbody.xml"
 
-#
 images = [cv2.imread(file, cv2.IMREAD_UNCHANGED) for file in glob.glob("../data/A2/data/test/*.png")]
 file_names = os.listdir("../data/A2/data/test/")
 
 casc_body_path = cv2.CascadeClassifier(casc_body_path)
 
-for img, name in zip(images, file_names):    
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    faces = casc_body_path.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
-    # Draw a rectangle around the faces
+with open("../data/A2/data/result.txt", 'w') as res:
     
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0 ), 2)
+
+    for img, name in zip(images, file_names):    
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = casc_body_path.detectMultiScale(gray)
+        # Draw a rectangle around the faces
         
-    cv2.imwrite("../data/A2/data/out/out-" + name,img)
+        if len(faces) == 0:
+            res.write("0\n")
+        else:
+            res.write("1\n")
             
+        #for (x, y, w, h) in faces:
+        #    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0 ), 2)
             
+        
+            
+        #cv2.imwrite("../data/A2/data/out/out-" + name,img)
+                
+                
